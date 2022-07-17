269b5dcf431eeb215ac1fcc815e71c3670562482
Khamilah Nixon <knixon4@ggc.edu>
2022-07-17 15:58:46 -0400

    Inserts store location data into mydb schema

---
+++ StoreLocationData.sql
@@ -0,0 +1,31 @@
+use mydb;
+INSERT INTO `mydb`.`store location`(StoreID,Longitude,Latitude,Address,City,State,ZipCode)
+	VALUES(1,34,83,'3333 Buford Dr','Buford','GA',30519);
+INSERT INTO `mydb`.`store location`(StoreID,Longitude,Latitude,Address,City,State,ZipCode)
+	VALUES(2,33,84,'1350 Scenic Hwy S Suite 400','Snellville','GA',30078);
+INSERT INTO `mydb`.`store location`(StoreID,Longitude,Latitude,Address,City,State,ZipCode)
+	VALUES(3,33,84,'231 18th St NW','Atlanta','GA',30363);
+INSERT INTO `mydb`.`store location`(StoreID,Longitude,Latitude,Address,City,State,ZipCode)
+	VALUES(4,33,84,'4400 Ashford Dunwoody Rd','Atlanta','GA',30346);
+INSERT INTO `mydb`.`store location`(StoreID,Longitude,Latitude,Address,City,State,ZipCode)
+	VALUES(5,33,84,'5900 Sugarloaf Pkwy','Lawrenceville','GA',30043);
+INSERT INTO `mydb`.`store location`(StoreID,Longitude,Latitude,Address,City,State,ZipCode)
+	VALUES(6,33,84,'1000 Southlake Cir','Morrow','GA',30260);
+INSERT INTO `mydb`.`store location`(StoreID,Longitude,Latitude,Address,City,State,ZipCode)
+	VALUES(7,33,84,'1620 Cumberland Mall','Atlanta','GA',30339);
+INSERT INTO `mydb`.`store location`(StoreID,Longitude,Latitude,Address,City,State,ZipCode)
+	VALUES(8,33,83,'800 Steven B Tanger Blvd','Commerce','GA',30529);
+INSERT INTO `mydb`.`store location`(StoreID,Longitude,Latitude,Address,City,State,ZipCode)
+	VALUES(9,33,84,'2929 Turner Hill Rd Suite 2750','Lithonia','GA',30038);
+INSERT INTO `mydb`.`store location`(StoreID,Longitude,Latitude,Address,City,State,ZipCode)
+	VALUES(10,34,84,'1081 North Point Mall','Alpharetta','GA',30022);
+INSERT INTO `mydb`.`store location`(StoreID,Longitude,Latitude,Address,City,State,ZipCode)
+	VALUES(11,34,84,'6300 Shadburn Ferry Rd','Buford','GA',30518);
+INSERT INTO `mydb`.`store location`(StoreID,Longitude,Latitude,Address,City,State,ZipCode)
+	VALUES(12,33,82,'1 Freedom Way','Augusta','GA',30904);
+INSERT INTO `mydb`.`store location`(StoreID,Longitude,Latitude,Address,City,State,ZipCode)
+	VALUES(13,34,85,'1730 Lakeview Dr','Rossville','GA',30741);
+INSERT INTO `mydb`.`store location`(StoreID,Longitude,Latitude,Address,City,State,ZipCode)
+	VALUES(14,34,83,'919 Overflow Creek Rd','Clayton','GA',30525);
+INSERT INTO `mydb`.`store location`(StoreID,Longitude,Latitude,Address,City,State,ZipCode)
+	VALUES(15,30,83,'371 Gil Harbin Industrial Blvd','Valdosta','GA',31601);
